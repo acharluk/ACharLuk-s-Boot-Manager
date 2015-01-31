5c996561695a3ee0121a9c4d53de9c4a1274d57a
ACharLuk <acharluk@github.com>
2015-01-31 19:16:37 +0100

    Reading os configuration file

--- abm/abm.lua
+++ abm/abm.lua
@@ -4,30 +4,31 @@ local defaultConfigurationFile = "/config.abm"
 
 --[[ Tables ]]--
 local OsList = fs.list(defaultOsFolder)
-local OsBootFile = {}
+local OS = {
+	[1] = {name = "hoa", version = "1.2", boot = "asdsad.lua"}
+}
 
 --[[ Functions ]]--
 function listOs()
 	for i, os_folder in ipairs(OsList) do
-		if checkSuitableOs(os_folder) then
-			print(i..". "..os_folder)
-		end
+		checkSuitableOs(i, os_folder)
+	end
+	for i = 1, #OS do
+		print(i..". "..OS[i].name.."  Version: "..OS[i].version.."  Main file: "..OS[i].boot)
 	end
 end
 
-function checkSuitableOs(os_folder)
-	if fs.exists(defaultOsFolder..os_folder..defaultConfigurationFile) then
-		--Check configuration file
-		print("yep, suitable")
-		return true
-	else
-		print("Not a valid os")
-		return false
+function checkSuitableOs(index, os_folder)
+	file = defaultOsFolder..os_folder..defaultConfigurationFile
+	if fs.exists(file) then
+		f = fs.open(file, "r")
+		OS[index] = {name = string.sub(f.readLine(), 6), version = string.sub(f.readLine(), 9), boot = string.sub(f.readLine(), 6)}
+		f.close()
 	end
 end
 
 --[[ Main function ]]--
-function main( ... )
+function main()
 	listOs()
 	term.write("Select OS: ")
 	selection = read()
