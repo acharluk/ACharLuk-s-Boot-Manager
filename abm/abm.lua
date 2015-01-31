918af498c200972e815811c0d6039a9d71575d8d
ACharLuk <acharluk@github.com>
2015-01-31 12:02:36 +0100

    Created default os configuration file

--- abm/abm.lua
+++ abm/abm.lua
@@ -1,12 +1,28 @@
 --[[ Variables ]]--
+local defaultOsFolder = "os/"
+local defaultConfigurationFile = "/config.abm"
 
 --[[ Tables ]]--
-local OsList = fs.list("os/")
+local OsList = fs.list(defaultOsFolder)
+local OsBootFile = {}
 
 --[[ Functions ]]--
 function listOs()
 	for i, os_folder in ipairs(OsList) do
-		print(i..". "..os_folder)
+		if checkSuitableOs(os_folder) then
+			print(i..". "..os_folder)
+		end
+	end
+end
+
+function checkSuitableOs(os_folder)
+	if fs.exists(defaultOsFolder..os_folder..defaultConfigurationFile) then
+		--Check configuration file
+		print("yep, suitable")
+		return true
+	else
+		print("Not a valid os")
+		return false
 	end
 end
 
