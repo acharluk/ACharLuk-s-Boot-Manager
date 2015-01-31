47ea314628379e2058a5bde66bd4dfd6a680397c
ACharLuk <acharluk@github.com>
2015-01-31 11:39:40 +0100

    Setting up os listing

--- abm/abm.lua
+++ abm/abm.lua
@@ -1,1 +1,24 @@
-print("ACL Boot Manager")
+--[[ Variables ]]--
+
+--[[ Tables ]]--
+local OsList = fs.list("os/")
+
+--[[ Functions ]]--
+function listOs()
+	for i, os_folder in ipairs(OsList) do
+		print(i..". "..os_folder)
+	end
+end
+
+--[[ Main function ]]--
+function main( ... )
+	listOs()
+	term.write("Select OS: ")
+	selection = read()
+	--Run main file of the selected os
+end
+
+local ok, err = pcall(main)
+if not ok then
+	print(err)
+end
