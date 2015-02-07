d4c0db459a7c8897ac4f4d1887d65823de16dd40
ACharLuk <acharluk@gmail.com>
2015-02-07 17:52:40 +0100

    Fixes, changed the way os configuration searching works

--- abm/abm.lua
+++ abm/abm.lua
@@ -1,4 +1,5 @@
 --[[ Variables ]]--
+local running = true
 local ABMConfigFile = "configuration.cfg"
 
 -- Menu frame --
@@ -34,7 +35,6 @@ function splitstr(string, reg)
 end
 function loadConfiguration() 
 	f = fs.open("abm/"..ABMConfigFile, "r")
-	--i = 1
 	while true do
 		line = f.readLine()
 		if line == nil then break end
@@ -43,25 +43,22 @@ function loadConfiguration()
 	end
 	f.close()
 end
-function listOs()
+function loadOS()
 	OS = {}
-	for i, os_folder in ipairs(OsList) do
-		checkSuitableOs(i, os_folder)
-	end
-end
-
-function checkSuitableOs(index, os_folder)
-	file = conf["default_os_folder"]..os_folder..conf["default_conf_file"]
-	if fs.exists(file) then
-		f = fs.open(file, "r")
-		OS[index] = {
-			folder = conf["default_os_folder"]..os_folder,
-			name = string.sub(f.readLine(), 6),
-			version = string.sub(f.readLine(), 9),
-			boot = string.sub(f.readLine(), 6),
-			line = 0
-		}
-		f.close()		
+	OsList = fs.list(conf["default_os_folder"])
+	for i, os_folder in pairs(OsList) do
+		path = conf["default_os_folder"]..os_folder..conf["default_conf_file"]
+		if fs.exists(path) then
+			f = fs.open(path,"r")
+			t = #OS
+			OS[t + 1] = {
+				folder = conf["default_os_folder"]..os_folder,
+				name = splitstr(f.readLine(), "=")[2],
+				version = splitstr(f.readLine(), "=")[2],
+				boot = splitstr(f.readLine(), "=")[2],
+			}
+			f.close()
+		end
 	end
 end
 
@@ -69,11 +66,10 @@ end
 	Draw main menu
 ]]--
 function drawMenu()
-
 	acl.cc(tonumber(conf["color_tx_inactive"]), tonumber(conf["color_bg_inactive"]))
 	acl.cls()
 	selected = 1
-	while true do
+	while running do
 		acl.drawFrame(w,h,shor,sver,scor)
 		acl.scp(9,2)
 		write("ACL Boot Manager "..conf["version"])
@@ -83,18 +79,18 @@ function drawMenu()
 		end
 		ev, k, _, line = os.pullEvent()
 		if ev == "key" then
-			if k == keys.up and selected > 1 then
+			if k == keys.up and selected > 1 and tonumber(conf["enable_touch"]) == 0 then
 				selected = selected - 1
-			elseif k == keys.down and selected < #OS then
+			elseif k == keys.down and selected < #OS and tonumber(conf["enable_touch"]) == 0 then
 				selected = selected + 1
-			elseif k == keys.enter then
+			elseif k == keys.enter and tonumber(conf["enable_touch"]) == 0 then
 				launch = OS[selected].folder.."/"..OS[selected].boot
 				acl.cls(1, 1)
 				shell.run(launch)
 				return
 			elseif k == keys.r then
 				acl.cls()
-				listOs()
+				loadOS()
 			end
 		elseif ev == "mouse_click" then
 			for i = 1, #OS do
@@ -116,16 +112,11 @@ end
 ]]--
 function menu(id, text)
 	if selected == id then
-		if tonumber(conf["enable_key"]) == 1 then
-			write("  ")
-		elseif tonumber(conf["enable_mouse"]) == 1 then
+		if tonumber(conf["enable_touch"]) == 0 then
 			acl.cc(tonumber(conf["color_tx_active"]), tonumber(conf["color_bg_active"]))
 			write("* ")
 		else
-			print("Please select an input method")
-			print("Press any key to restart")
-			os.pullEvent("key")
-			os.reboot()
+			write("  ")
 		end
 	else
 		write("  ")
@@ -140,9 +131,10 @@ function main()
 	os.loadAPI("apis/acl")
 	w, h = acl.size()
 	loadConfiguration()
-	OsList = fs.list(conf["default_os_folder"])
+	
 	print()
-	listOs()
+	--listOs()
+	loadOS()
 	drawMenu()
 end
 
