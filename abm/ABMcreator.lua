0a95ecc64dadcf6df656eac150522ba3f349e3e8
ACharLuk <acharluk@gmail.com>
2015-02-08 09:14:34 +0100

    Tweaking ABM creator

--- abm/ABMcreator.lua
+++ abm/ABMcreator.lua
@@ -1,21 +1,51 @@
 local tArgs = { ... }
 
-mode = tArgs[1]
-if mode == "create" then
-	folder = tArgs[2]
-	name = tArgs[3]
-	version = tArgs[4]
-	boot = tArgs[5]
-
+function create(folder, name, version, boot)
 	fs.makeDir("os/"..folder)
 	file = fs.open("os/"..folder.."/config.abm", "w")
 	file.writeLine("name="..name)
 	file.writeLine("version="..version)
 	file.writeLine("boot="..boot)
 	file.close()
-elseif mode == "delete" then
-	folder = tArgs[2]
+end
+function delete(folder)
 	fs.delete("os/"..folder)
-else
-	printError("Usage: ABMcreator.lua [create/delete] folder name version boot")
+end
+mode = tArgs[1]
+function main()
+	os.loadAPI("apis/acl")
+	if #tArgs > 0 then
+		if mode == "create" then
+			create(tArgs[2], tArgs[3], tArgs[4], tArgs[5])
+		elseif mode == "delete" then
+			delete(tArgs[2])
+		else
+			printError("Usage: ABMcreator.lua [create/delete] folder name version boot")
+		end
+	else
+		acl.cls(1, 1)
+		print("ABM os configuration file creator")
+		
+		write("Enter folder: ")
+		folder = read()
+		print()
+
+		write("Enter name: ")
+		name = read()
+		print()
+
+		write("Enter version: ")
+		version = read()
+		print()
+
+		write("Enter boot: ")
+		boot = read()
+		print()
+
+		create(folder, name, version, boot)
+	end
+end
+local ok, err = pcall(main)
+if not ok then
+	print(err)
 end
